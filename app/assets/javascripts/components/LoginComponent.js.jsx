9283f2ebfcb95541af9cd80698a42f781b61f9e9
ChrisDJordan <chrisjordan740@gmail.com>
2015-06-18 18:57:25 -0500

    added main and header form

--- app/assets/javascripts/components/LoginComponent.js.jsx
+++ app/assets/javascripts/components/LoginComponent.js.jsx
@@ -1,11 +1,51 @@
 var Profile = React.createClass({
 	render: function () {
 		return (
-		  <div>
-			<h1> Profile Page </h1>
-			<a href='#edit'>Edit</a>
-			<input type="text"/>
-		  </div>
-		);
+            <div class="header-sign-in">
+                <form>
+                    <label>
+                        Username
+                        <input type="text" name="username">
+                    </label>
+    
+             		<label>
+      			        Password
+      					<input type="password" name="password">
+    				</label>
+   
+    				<input class="btn" type="submit" name="submit" value="Sign In">
+				</form>
+            </div>
+
+			<div class="sign-up">
+				<form>
+			        <fieldset class="sign-up-info">
+						  <label>
+						      Email
+						      <input type="email" name="email-address"/>
+                          </label>
+
+                          <label>
+                              Username
+                              <input type="text" name="username"/>    
+                          </label>
+
+                          <label>
+                              Password
+							  <input type="password" name="password"/>
+						  </label>
+
+						  <label>
+      					      Confirm Password
+      						  <input type="password" name="password"/>
+    					  </label>
+  					</fieldset>
+  
+                    <fieldset class="sign-up-action">
+    			        <input class="btn" type="submit" name="submit" value="Sign Up">
+                    </fieldset>
+				</form>
+            </div
+      );
 	}
 });
