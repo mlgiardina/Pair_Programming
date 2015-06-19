7a5a8119e5bab14807d79dc97a0a6886d9a50997
Matthew Giardina <mlgiardina@me.com>
2015-06-19 15:02:27 -0500

    fixed missing 'message' in messages controller/create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,12 @@ class UsersController < ApplicationController
     if User.exists?(username: params[:username])
       render json: { message: "That username already exists. Please pick another one." }
     else
-      user = User.create(user_params)
-      render json: user
+      user = User.new(user_params)
+      if user.save
+        render json: user
+      else
+        render json: { message: "Error" }
+      end
     end
   end
 
