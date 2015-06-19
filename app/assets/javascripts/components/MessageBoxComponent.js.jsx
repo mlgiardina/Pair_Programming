46859f81f928e92b6d4d5805ccb6326938be1785
allen wes <allen.wes.g@gmail.com>
2015-06-19 16:51:52 -0500

    adding messages functionality

--- app/assets/javascripts/components/MessageBoxComponent.js.jsx
+++ app/assets/javascripts/components/MessageBoxComponent.js.jsx
@@ -3,7 +3,7 @@ var MessageBox = React.createClass({
 	render: function(){
 		var userMessages = this.props.receivedMessages.map(function(obj){
 
-			return (<div key={obj.id}>{obj.sender_name}: {obj.body}</div>);
+			return (<div key={obj.id}><a href="#" onCLick={this.displaySpecifcMessages}>{obj.sender_name}</a></div>);
 		});
 		return (
 			<div>
