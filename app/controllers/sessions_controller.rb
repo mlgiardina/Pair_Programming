8c1b575a7945a23367c4598986212214cbbb72ee
Matthew Giardina <mlgiardina@me.com>
2015-06-19 11:27:52 -0500

    added login and out routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Successfully logged out. Goodbye!"
+    redirect_to root_url
   end
 end
