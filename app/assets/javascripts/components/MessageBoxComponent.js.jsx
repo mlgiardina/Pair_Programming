cf27eb390e096a264a1e75713cb838378c5e630f
allen wes <allen.wes.g@gmail.com>
2015-06-19 17:35:39 -0500

    added algorithm for messageBox

--- app/assets/javascripts/components/MessageBoxComponent.js.jsx
+++ app/assets/javascripts/components/MessageBoxComponent.js.jsx
@@ -1,27 +1,38 @@
 var MessageBox = React.createClass({
 
 	render: function(){
-		var userMessages = this.props.receivedMessages.map(function(obj){
-			var myObj = {};
-			for(var prop in obj){
-				if(prop === "sender_name"){
-					if(myObj.hasOwnProperty(prop){
-						continue;
-					} else {
-						myObj[prop] = [];
-					}
-					
+		var received = this.props.receivedMessages;
+		console.log(received);
+		var myObj = {};
+		    for(var i = 0; i < received.length; i++){
+		        for(var prop in received[i]){
+		        	if(prop === "sender_name"){
+		        		if(myObj.hasOwnProperty(prop)){
+		        			continue;
+		        		} else {
+		        			myObj[received[i][prop]] = [];
+		        		}
+		        	}
+		        }
+		    }
+		    for(var prop in myObj){
+		        for(var i = 0; i < received.length; i++){
+		            if(received[i]["sender_name"] === prop){
+		                myObj[prop].push(received[i]["body"])
+		            }
+		        }
+		    }
 
-				}
+		    console.log(myObj);
+		    for(var thing in myObj){
+				var userMessages = myObj[thing].map(function(obj){
+					console.log(obj);
+					return (<div>{thing}:{obj}</div>);
+				});
 			}
-			return (<div key={obj.id}><a href="#" onCLick={this.displaySpecifcMessages}>{obj.sender_name}</a></div>);
-		});
 		return (
 			<div>
-				{this.props.user}
-					<div>
-						{userMessages}
-					</div>
+				{userMessages}
 			</div>
 		);
 	}
