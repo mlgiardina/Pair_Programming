c6700f2e9ee23d0bd60246f5db758f6bcb60ed80
ChrisDJordan <chrisjordan740@gmail.com>
2015-06-20 18:51:27 -0500

    made more border radius changes to columns

--- app/assets/javascripts/components/ProfilePageComponent.js.jsx
+++ app/assets/javascripts/components/ProfilePageComponent.js.jsx
@@ -9,63 +9,50 @@ var ProfilePage = React.createClass({
 			<div>
 
 			<div className="header-color col12">
-			   <h1>Pair Programming</h1>
-
-
-			   
-				<button className="button-right" onClick={this.logOut}>Logout</button>
-			
-				
-				
-
-			
-				
-				<div>
+				<h1>Pair Programming</h1>
+                <button className="button-right" onClick={this.logOut}>Logout</button>
+			<div>
 					<div id="target-messagebox"></div>
-				</div>
+            </div>
+
 			</div>	
 
           
 			<div className="col12 color">
-                <div className="col2 delete add"></div>
+            	<div className="col2 delete add"></div>
 
                 <div className="profile-img col3">
-                        <img src="http://fillmurray.com/400/400"/>
+                	<img src="http://fillmurray.com/400/400"/>
                 </div>
 
                 <div className=" border profile-bio col5">
                     
-               		<h2>Bio</h2>
+                	<h2>Bio</h2>
                		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only </p>
-               		</div>
+                </div>
             
 
 
-              </div>
-
-              <div className="col12 color">
-                  <div className=" col2 add delete"></div>
-                  <div className=" border matches-likes col4">
-                      <h2>Matches</h2>
-
-                  </div>
-                  <div className=" border matches-likes col4">
-                      <h2>Likes</h2>
-                  </div>
-
-             </div>
-
-             <div className=" col12 color">
-
-             <div className=" col2 add delete"></div>
-
-               <div className="border col8 questions-section">
-                   <h2>Questions</h2>
-
-
-               </div>
-                <div className="col2 add delete"></div>
                 </div>
+
+                <div className="col12 color">
+                	<div className=" col2 add delete"></div>
+                    <div className=" border matches-likes col4">
+                      <h2>Matches</h2>
+                </div>
+
+                <div className=" border matches-likes col4">
+                	<h2>Likes</h2>
+                </div>
+
+                </div>
+
+				<div className=" col12 color">
+					<div className=" col2 add delete"></div>
+					<div className="border col8 questions-section">
+                   		<h2>Questions</h2>
+				   </div>
+				</div>
 
       
          
