3b5e68a8f6f978a2b8ce2ba2b5b23db155a35154
ChrisDJordan <chrisjordan740@gmail.com>
2015-06-20 18:34:49 -0500

    added border radius to columns

--- app/assets/javascripts/components/ProfilePageComponent.js.jsx
+++ app/assets/javascripts/components/ProfilePageComponent.js.jsx
@@ -4,18 +4,119 @@ var ProfilePage = React.createClass({
 			return (<div key={question}>{question}</div>);
 		});
 		return (
+
+			// profile page html--------------------------------
 			<div>
-				<button onClick={this.logOut}>Logout</button>
-				<button onClick={this.showMessageComponent}>Send Message</button>
-				hi {this.props.user}!
-				<a onClick={this.displayMessageBox} href="#">Message</a>
-				<div id="send-message">
-				</div>
+
+			<div className="header-color col12">
+			   <h1>Pair Programming</h1>
+
+
+			   
+				<button className="button-right" onClick={this.logOut}>Logout</button>
+			
+				
+				
+
+			
+				
 				<div>
 					<div id="target-messagebox"></div>
-					<h1>Questionare</h1>
-					{questionare}
 				</div>
+			</div>	
+
+          
+			<div className="col12 color">
+                <div className="col2 delete add"></div>
+
+                <div className="profile-img col3">
+                        <img src="http://fillmurray.com/400/400"/>
+                </div>
+
+                <div className=" border profile-bio col5">
+                    
+               		<h2>Bio</h2>
+               		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only </p>
+               		</div>
+            
+
+
+              </div>
+
+              <div className="col12 color">
+                  <div className=" col2 add delete"></div>
+                  <div className=" border matches-likes col4">
+                      <h2>Matches</h2>
+
+                  </div>
+                  <div className=" border matches-likes col4">
+                      <h2>Likes</h2>
+                  </div>
+
+             </div>
+
+             <div className=" col12 color">
+
+             <div className=" col2 add delete"></div>
+
+               <div className="border col8 questions-section">
+                   <h2>Questions</h2>
+
+
+               </div>
+                <div className="col2 add delete"></div>
+                </div>
+
+      
+         
+
+
+			
+
+
+
+
+		
+	
+		
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
 			</div>
 			//TODO build out nice look profile page
 		);
