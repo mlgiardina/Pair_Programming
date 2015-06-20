0180fde4f801beb848323f7d84a04f7ac8f31d20
allen wes <allen.wes.g@gmail.com>
2015-06-19 21:24:21 -0500

    finished algorithm

--- app/assets/javascripts/components/MessageBoxComponent.js.jsx
+++ app/assets/javascripts/components/MessageBoxComponent.js.jsx
@@ -5,27 +5,27 @@ var MessageBox = React.createClass({
 		console.log(received);
 		var myObj = {};
 		var propHolding = [];
-		    for(var i = 0; i < received.length; i++){
-		        for(var prop in received[i]){
-		        	if(prop === "sender_name"){
-		        		if(myObj.hasOwnProperty(prop)){
-		        			continue;
-		        		} else {
-		        			myObj[received[i][prop]] = [];
-		        		}
-		        	}
-		        }
-		    }
-		    for(var prop in myObj){
-		        for(var i = 0; i < received.length; i++){
-		            if(received[i]["sender_name"] === prop){
-		                myObj[prop].push(received[i]["body"])
-		            }
-		        }
-		    }
+			for(var i = 0; i < received.length; i++){
+				for(var prop in received[i]){
+					if(prop === "sender_name"){
+						if(myObj.hasOwnProperty(prop)){
+							continue;
+						} else {
+							myObj[received[i][prop]] = [];
+						}
+					}
+				}
+			}
+			for(var prop in myObj){
+				for(var i = 0; i < received.length; i++){
+					if(received[i]["sender_name"] === prop){
+						myObj[prop].push(received[i]["body"])
+					}
+				}
+			}
 
-		    console.log(myObj);
-		    for(var thing in myObj){
+			console.log(myObj);
+			for(var thing in myObj){
 				propHolding.push(thing);
 			}
 			var headers = propHolding.map(function(header){
@@ -36,10 +36,11 @@ var MessageBox = React.createClass({
 				
 				return (<div>{header}:{body}</div>);
 			});
-		return (
-			<div>
-				{headers}
-			</div>
+			return (
+				<div>
+					{headers}
+					<br/>
+				</div>
 		);
 	}
 
