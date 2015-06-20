1e9db4da21d867d7f955d20b49a3355b6a80777f
allen wes <allen.wes.g@gmail.com>
2015-06-20 17:23:39 -0500

    added photo upload component, and functionality to updat profile info

--- app/assets/javascripts/components/BioFormComponent.js.jsx
+++ app/assets/javascripts/components/BioFormComponent.js.jsx
@@ -1,11 +1,34 @@
+var userToUpdate;
 var BioForm = React.createClass({
+	componentWillMount: function(){
+		$.get("http://localhost:3000/session",function(data){
+			userToUpdate = data;
+		})
+	},
 	render: function(){
 		return (<div>
-					Name: <input type="text" />
-					Email: <input type="text" />
-					Bio: <textarea></textarea>
-					<UploadPhoto/>
+					Name: <input ref="updateName" type="text" /><br/>
+					Email: <input ref="updateEmail" type="text" /><br/>
+					Bio: <textarea ref="updateBio"></textarea><br/>
+					<button onClick={this.saveProfileInfo}>Save</button>
+					<UploadPhoto ref="updatePicture" />
 				</div>
 		);
+	},
+	saveProfileInfo: function(){
+		$.ajax(
+			type:"PUT",
+			url:"http://localhost:3000/users",
+			data:{user:
+					{
+					id: userToUpdate.id,
+					picture:this.refs.updatePicture.getDOMNode().value, 
+					bio: this.refs.updateBio.getDOMNode().value, 
+					email: this.refs.updateEmail.getDOMNode().value, 
+					name: this.refs.updateName.getDOMNode().value
+				}},
+			success: function(){
+				console.log("i work!")
+			});
 	}
 });
--- app/assets/javascripts/components/ProfilePicUploadComponent.js.jsx
+++ app/assets/javascripts/components/ProfilePicUploadComponent.js.jsx
@@ -1,9 +1,13 @@
 var UploadPhoto = React.createClass({
 	render: function(){
-		return (<div><input type="text"/><button onCLick={this.uploadPhoto}>Upload</button></div>);
+		return (<div><input type="text"/><button onClick={this.uploadPhoto}>Upload</button>
+				<img ref="anImage" src="#"/></div>);
 	},
-	uploadPhoto: function(){
+	uploadPhoto: function(event){
+		event.preventDefault();
+		var image = this.refs.anImage;
 		$.get("http://localhost:3000/likes", function(data){
+			console.log(image.getDOMNode().src = data.url);
 			console.log(data.url);
 		})
 	}
