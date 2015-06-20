90ba894161c58f46b28d7265a02518fedbb60098
Matthew Giardina <mlgiardina@me.com>
2015-06-20 15:05:13 -0500

    added questions to main.js.jsx

--- app/assets/javascripts/main.js.jsx
+++ app/assets/javascripts/main.js.jsx
@@ -1,9 +1,18 @@
 
 var containerEl = document.getElementById("container");
-var questions = ["Question 1","Question 2","Question 3","Question 4","Question 5","Question 6","Question 7",
-"Question 8","Question 9","Question 10","Question 11","Question 12","Question 13","Question 14","Question 15",
-"Question 16","Question 17","Question 18","Question 19","Question 20","Question 21","Question 22","Question 23",
-"Question 24","Question 25"];
+var questions = [
+	"1.) Which OS do you prefer?",
+	"2.) I love TDD. Agree or disagree?",
+	"3.) What is your preferred text editor color scheme?",
+	"4.) Do you prefer the front-end or back-end?",
+	"5.) Do you know Fortran?","People who choose regular expressions to solve a problem inevitably end up with more problems. Agree or disagree?",
+	"6.) Mouse, trackpad, or VIM?",
+	"7.) Bracket notation or literal notation?",
+	"8.) Spaces or tabs?",
+	 "9.) Which is your preferred browser?",
+	"10.) Startup, enterprise, or freelance?"
+];
+
 var App = Backbone.Router.extend({
 	routes: {
 		"":"login",
@@ -13,11 +22,11 @@ var App = Backbone.Router.extend({
 	},
 	login: function() {
 		React.render(<Login routing={myRouter}/>, containerEl);
-	}, 
+	},
 	profile: function(user){
 		React.render(<ProfilePage routing={myRouter} questions={questions} user={user} />, containerEl);
 	}
 });
 
 var myRouter = new App();
-Backbone.history.start();   
+Backbone.history.start();
