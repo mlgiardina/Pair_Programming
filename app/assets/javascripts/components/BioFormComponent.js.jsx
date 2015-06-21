c586c2bb9d4c4094b61954eb1aa597c13261f5cc
allen wes <allen.wes.g@gmail.com>
2015-06-21 15:15:54 -0500

    pulling from master

--- app/assets/javascripts/components/BioFormComponent.js.jsx
+++ app/assets/javascripts/components/BioFormComponent.js.jsx
@@ -7,7 +7,7 @@ var BioForm = React.createClass({
 					Email: <input ref="updateEmail" type="text" defaultValue={userToUpdate.email}/><br/>
 					Bio: <input ref="updateBio" defaultValue={userToUpdate.bio}/><br/>
 					<button onClick={this.saveProfileInfo}>Save</button>
-					<UploadPhoto userPic={userToUpdate.picture} ref="updatePicture" />
+					<input ref="userImageUpload" type="text"/><button onClick={this.uploadPhoto}>Upload</button>
 				</div>
 		);
 	},
@@ -18,13 +18,30 @@ var BioForm = React.createClass({
 			data:{user:
 					{
 					username: userToUpdate.username,
-					picture:this.refs.updatePicture.getDOMNode().src, 
+					picture: this.refs.userImageUpload.getDOMNode().value,
 					bio: this.refs.updateBio.getDOMNode().value, 
 					email: this.refs.updateEmail.getDOMNode().value, 
 					name: this.refs.updateName.getDOMNode().value
 				}},
 			success: function(){
 				React.render(<ProfilePage routing={myRouter} questions={questions} profileName={userToUpdate.name} />, containerEl);
+			}
+		});
+	},
+	uploadPhoto: function(event){
+		event.preventDefault();
+		console.log("im running!");
+		var image = this.refs.userImageUpload.getDOMNode().value;
+		$.ajax({
+			type:"PUT",
+			url:"http://localhost:3000/users",
+			data:{user:
+					{
+					picture: image
+				}},
+			success: function(){
+				console.log("i ran!!");
+				//React.render(<ProfilePage routing={myRouter} questions={questions} profileName={userToUpdate.name} />, containerEl);
 			}
 		});
 	}
