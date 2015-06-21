5c489edea7a3b5986d9352026b18a4545d11ea8f
Matthew Giardina <mlgiardina@me.com>
2015-06-21 12:25:59 -0500

    set up serialization for answer body

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,13 +4,22 @@ class AnswersController < ApplicationController
     render json: Answer.all
   end
 
+  def show
+    answer = Answer.where(user_id: params[:user_id]).first
+    answer.body = answer.body.split["~"]
+    render json: { }
+  end
+
   def create
     authenticate_user!
-    answer = Answer.new(answer_params)
+    serialized_body = params[:answer][:body].join("~")
+    answer = Answer.new(user_id: params[:answer][:user_id], body: serialized_body)
     if answer.save
       render json: { message: "questions answered" }
     else
       render json: { message: "error" }, status: 403
+    end
+
   end
 
   private
--- db/migrate/20150618220350_create_answers.rb
+++ db/migrate/20150618220350_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.belongs_to :user, index: true, foreign_key: true, null: false
-      t.string :body, null: false
+      t.text :body, null: false
 
       t.timestamps null: false
     end
