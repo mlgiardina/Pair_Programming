acd00ab12466bf98dec39a11c7d1485063705b6d
Matthew Giardina <mlgiardina@me.com>
2015-06-21 20:22:23 -0500

    added matches model/controller and configured routes for it

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,42 @@
+class MatchesController < ApplicationController
+
+  def index
+    user = User.where(username: params[:username]).first
+    render json: user.matches.order_by.reverse.limit(3)
+  end
+
+  def create
+    serialized_match_body = params[:match][:body].join("~")
+    match = Match.new(user_id: params[:match][:user_id], body: serialized_match_body)
+    if match.save
+      render json: match
+    else
+      render json: { message: "error" }, status: 403
+    end
+  end
+
+  def update
+    matches = User.find(session[:user_id]).matches
+    if matches.update(match_params)
+      render json: matches
+    else
+      render json: { message: "error" }, status 403
+    end
+  end
+
+  def show
+    sorted_matches = []
+    matches = User.find(session[:user_id]).matches
+    matches.each do |match|
+      unserialized_match_body = match.body.split("~")
+      sorted_matches.push(unserialized_match_body)
+    end
+    render json: sorted_matches.sort_by { |match| match[1] }.reverse
+
+  private
+
+  def match_params
+    params.require(:match).permit(:user_id, :body)
+  end
+
+end
---
+++ db/migrate/20150622000834_create_matches.rb
@@ -0,0 +1,10 @@
+class CreateMatches < ActiveRecord::Migration
+  def change
+    create_table :matches do |t|
+      t.belongs_to :user, index: true, foreign_key: true
+      t.text :body
+
+      t.timestamps null: false
+    end
+  end
+end
